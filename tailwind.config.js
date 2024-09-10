0d53838e052c2e9ee70f046e58e621f19a43f16d
Bikram Basnett <Bikrambasnett7@gmail.com>
2024-09-10 13:59:32 +0530

    Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       fontFamily:{
         rubik:['Rubik' ,'sans-serif'],
         roboto:['Roboto' ,'sans-serif'],
-      }
+      },
+
+      boxShadow:{
+      custom:['rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px'],
+      },
     },
   },
   plugins: [],
